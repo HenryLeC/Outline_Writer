eb6715bcdc0ab72eca6e55e50fd41b735f35dfd4
Henry LeCompte <lecompteh18@gmail.com>
2020-05-22 17:07:09 -0400

    Version 1

---
+++ Writer.py
@@ -0,0 +1,48 @@
+import string
+
+NUMERALS = ['I', 'II', 'III', 'IV', 'V', 'VI']
+LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+lesson = 0
+header = 1
+sentence = 1
+
+Text = ''
+
+Text += NUMERALS[lesson] + '. ' + input("Lesson Name: ") + '\n'
+lesson += 1
+
+Text += '    ' + str(header) + '. ' + input('Header: ') + '\n'
+header += 1
+
+while True:
+    try:
+        Text += '        ' + str(sentence) + ', ' + input('Sentence: ') + '\n'
+    except KeyboardInterrupt:
+        Command = input('L for new Lesson, H for new header, Q for quit: ').lower()
+        if Command == 'l':
+            Text += NUMERALS[lesson] + '. ' + input("Lesson Name: ") + '\n'
+            lesson += 1
+            header = 1
+            sentence = 1
+        elif Command == 'h':
+            Text += '    ' + str(header) + '. ' + input('Header: ') + '\n'
+            header += 1
+            sentence = 1
+        elif Command == 'q':
+            with open("Outline.txt", "a", encoding="utf-8") as f:
+                f.write(Text)
+                f.close()
+            quit()
+        else:
+            pass
+
+
+
+
+
+
+
+
+
+
